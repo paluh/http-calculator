c03ea1c2d67bea528aeb5ae0c349fa8a0f0d729d
Tomasz Rybarczyk <paluho@gmail.com>
2018-07-04 18:36:59 +0200

    Cleanup import list

--- src/Main.hs
+++ src/Main.hs
@@ -14,34 +14,19 @@ import Prelude ()
 import Prelude.Compat
 
 import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Aeson.Compat
 import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import Data.ByteString (ByteString)
 import Data.IORef (atomicModifyIORef', IORef, newIORef, modifyIORef, readIORef, writeIORef)
-import Data.List
-import Data.Maybe
-import Data.String.Conversions
-import Data.Time.Calendar
 import GHC.Generics
-import Lucid
-import Network.HTTP.Media ((//), (/:))
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
-import System.Directory
-import Text.Blaze
-import Text.Blaze.Html.Renderer.Utf8
-import qualified Data.Aeson.Parser
-import qualified Text.Blaze.Html
 
-type State = [Int]
+type Db = [Int]
 data ResponseBody = ResponseBody { result :: Int, operands :: [Int] }
   deriving (Eq, Show, Generic)
 instance ToJSON ResponseBody
 
-toResponseBody :: State -> ResponseBody
+toResponseBody :: Db -> ResponseBody
 toResponseBody ops = ResponseBody (sum ops) ops
 
 type Calculator
@@ -49,15 +34,15 @@ type Calculator
   :<|> "sub" :> Capture "y" Int :> Get '[JSON] ResponseBody
   :<|> "reset" :> Get '[JSON] ResponseBody
 
-add :: IORef State -> Int -> IO ResponseBody
+add :: IORef Db -> Int -> IO ResponseBody
 add db value =
   atomicModifyIORef' db (\ops -> let n = (value : ops) in (n, toResponseBody n))
 
-sub :: IORef State -> Int -> IO ResponseBody
+sub :: IORef Db -> Int -> IO ResponseBody
 sub db value =
   atomicModifyIORef' db (\ops -> let n = ((-1) * value : ops) in (n, toResponseBody n))
 
-reset :: IORef State -> IO ResponseBody
+reset :: IORef Db -> IO ResponseBody
 reset db =
   atomicModifyIORef' db (const $ ([], toResponseBody []))
 
